8af81ecc8777dc1e46299b9164786616a7b0a99f
tech-gian <sdi1900059@di.uoa.gr>
2020-11-13 00:27:00 +0200

    main create

---
+++ Exercise2/main.cc
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////
+// File with main function, to run the program
+//////////////////////////////////////////////
+
+
+// Header file include
+#include "classes.h"
+
+
+int main(int argc, char* argv[]) {
+    
+    // Arguments from command line
+    int K = atoi(argv[1]);
+    int L = atoi(argv[2]);
+    double Tquiet = atof(argv[3]);
+    double Tmessy = atof(argv[4]);
+
+
+
+    return 0;
+}
