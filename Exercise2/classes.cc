6ee00d32bdb20be99174cf15f99f0e32c931813b
tech-gian <sdi1900059@di.uoa.gr>
2020-11-12 12:36:49 +0200

    Sequences create

--- Exercise2/classes.cc
+++ Exercise2/classes.cc
@@ -29,8 +29,85 @@ void child::print(void) const {
 
 
 
-// Classroom Functions
+// Couple Functions
+
+couple::couple(child* boy, child* girl) {
+    this->boy = boy;
+    this->girl = girl;
+
+    cout << "A New Couple has been created" << endl;
+}
+
+couple::~couple() {
+    cout << "A Couple to be destroyed" << endl;
+}
 
 
+// Seq_chi Functions
 
+seq_chi::seq_chi(child** children, int size, int no) {
+
+    int i=0, j=0;
+    child** boys = new child*[size];
+    child** girls = new child*[size];
+    for (int k=0 ; k<size ; k++) {
+        if (children[k]->get_gen() == 'b') {
+            boys[i] = children[k];
+            i++;
+        }
+        else {
+            girls[j] = children[k];
+            j++;
+        }
+    }
+
+    int couples_size;
+    if (i > j) couples_size = i;
+    else couples_size = j;
+    this->couples = new couple*[couples_size];
+
+    for (int k=0 ; k<couples_size-1 ; k++) {
+        couple* temp = new couple(boys[k], girls[k]);
+        this->couples[k] = temp;
+    }
+
+    couple* temp;
+    if (i == j) {
+        temp = new couple(boys[i-1], girls[j-1]);
+    }
+    else if (i > j) {
+        temp = new couple(boys[i-1], NULL);
+    }
+    else {
+        temp = new couple(NULL, girls[j-1]);
+    }
+
+    this->couples[couples_size-1] = temp;
+
+    // Delete temp boys, girls
+    delete[] boys;
+    delete[] girls;
+
+    // Define size of couples
+    this->size = couples_size;
+    this->no = no;
+}
+
+
+seq_chi::~seq_chi() {
+    for (int i=0 ; i<this->size ; i++) {
+        delete this->couples[i];
+    }
+
+    delete[] this->couples;
+
+    cout << "A Seq_chi to be destroyed" << endl;
+}
+
+
+// Seq_seq Functions
+
+seq_seq::seq_seq(seq_chi** seqs, int size) {
+
+}
 
--- Exercise2/classes.h
+++ Exercise2/classes.h
@@ -15,6 +15,9 @@ using namespace std;
 class child {
     string name;    // Child's name
     int class_no;   // Child's classroom
+
+    // 'b' -> boy
+    // 'g' -> girl
     char gender;    // Child's gender
 
 
@@ -24,12 +27,62 @@ class child {
     child(string name, int class_no, char gender);
     // Destructor
     ~child();
+    // Getting child's gender
+    char get_gen(void) const { return this->gender; }
     // Print child info
     void print(void) const;
 
 };
 
 
+
+// Couple
+class couple {
+    child* boy;
+    child* girl;
+
+    public:
+    // Constructor
+    couple(child* boy, child* girl);
+    // Destructor
+    ~couple();
+
+};
+
+
+
+// Sequence of couples
+class seq_chi {
+    couple** couples;   // Array of couple*
+    int size;           // Size of array
+    int no;             // Sequence's number
+
+    public:
+    // Constructor
+    seq_chi(child** children, int size, int no);
+    // Destructor
+    ~seq_chi();
+
+};
+
+
+
+// Sequence of sequences of couples
+class seq_seq {
+    seq_chi** seqs;     // Array of seq_chi
+    int size;           // Size of array
+
+
+    public:
+    seq_seq(seq_chi** seqs, int size);
+    ~seq_seq();
+
+};
+
+
+
+
+// TO BE deleted
 // Classroom
 class classroom {
     int no;             // Classroom's number
