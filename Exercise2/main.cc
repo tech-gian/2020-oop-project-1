bc8660c7e9d7c23dbf0bf3e5aa7b9102326979c5
tech-gian <sdi1900059@di.uoa.gr>
2020-11-14 15:33:34 +0200

    main continue

--- Exercise2/main.cc
+++ Exercise2/main.cc
@@ -12,8 +12,8 @@ int main(int argc, char* argv[]) {
     // Arguments from command line
     int K = atoi(argv[1]);
     int L = atoi(argv[2]);
-    double Tquiet = atof(argv[3]);
-    double Tmessy = atof(argv[4]);
+    int Tquiet = atoi(argv[3]);
+    int Tmessy = atoi(argv[4]);
 
 
     // Initialize srand
@@ -32,13 +32,51 @@ int main(int argc, char* argv[]) {
         children[i] = new child(str, no, (i%2==0) ? 'b' : 'g');
     }
 
+    // Get children in K classrooms
+    child*** chi_seq = new child**[K];
+    for (int i=0 ; i<K ; i++) {
+        chi_seq[i] = new child*[size];
+    }
+    int* pos = new int[K];
+    for (int i=0 ; i<K ; i++) pos[i] = 0;
 
+    for (int i=0 ; i<size ; i++) {
+        int temp = rand() % K;
+        chi_seq[temp][pos[temp]] = children[i];
+        pos[temp]++;
+    }
+
+    // Get children in sequences (classrooms)
+    seq_chi** seqs = new seq_chi*[K];
+    for (int i=0 ; i<K ; i++) {
+        seq_chi* temp = new seq_chi(chi_seq[i], pos[i], i);
+        seqs[i] = temp;
+    }
+
+    // Get sequences_of_classrooms in a sequence
+    seq_seq* seq = new seq_seq(seqs, K, Tquiet, Tmessy);
+
+    // Print seq
+    seq->print();
 
 
 
 
 
     // Delete data
+    for (int i=0 ; i<K ; i++) {
+        delete chi_seq[i];
+    }
+    delete[] chi_seq;
+    delete[] pos;
+
+    for (int i=0 ; i<K ; i++) {
+        delete seqs[i];
+    }
+    delete[] seqs;
+
+    delete[] seq;
+
     for (int i=0 ; i<size ; i++) {
         delete children[i];
     }
